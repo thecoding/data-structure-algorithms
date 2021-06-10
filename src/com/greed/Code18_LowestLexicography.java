b679c6a412d9c423462c3aab4cd404d809965837
Mirko <mirko@MirkodeMacBook-Pro.local>
2021-06-10 10:59:05 +0800

    数组排序

--- src/com/greed/Code18_LowestLexicography.java
+++ src/com/greed/Code18_LowestLexicography.java
@@ -1,5 +1,7 @@
 package com.greed;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.TreeSet;
 
 /**
@@ -9,10 +11,7 @@ import java.util.TreeSet;
  */
 public class Code18_LowestLexicography {
 
-    public static void main(String[] args) {
-        String[] a = {"b","ab","cc"};
-        System.out.println(lowestString1(a));
-    }
+
 
     public static String lowestString1(String[] strs){
         if (strs == null || strs.length == 0) {
@@ -52,6 +51,77 @@ public class Code18_LowestLexicography {
         }
         return newArr;
     }
+
+
+    //暴力
+    // 1、生成随机的字符串数组、字符串长度随机
+    // 2、将所有的数据先排序，在组成字符串返回
+
+    // 生成随机数组
+    private static String[] generateRandomStringArray(int arrLen, int strLen){
+        int length = (int) (Math.random() * arrLen + 1);
+        String[] str = new String[length];
+        for (int i = 0; i < length; i++) {
+            str[i] = generateRandomString(strLen);
+        }
+        return str;
+    }
+
+    // 随机生成 65-90 97-122 的数字 中间相差25
+    private static String generateRandomString(int strLen) {
+        int length = (int)(Math.random() * strLen + 1);
+        char[] chars = new char[length];
+        for (int i = 0; i < length; i++) {
+            int value = (int)(Math.random() * 25 + 1);
+            chars[i] = (Math.random() <= 0.5) ? (char)(65 + value) : (char)(97 + value);
+        }
+        return String.valueOf(chars);
+    }
+
+    private static class MyComparator implements Comparator<String>{
+
+        @Override
+        public int compare(String a, String b) {
+            return (a + b).compareTo(b + a);
+        }
+    }
+
+    private static String lowestString2(String[] strs){
+        if(strs == null || strs.length == 0) {
+            return "";
+        }
+        Arrays.sort(strs,new MyComparator());
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < strs.length; i++) {
+            str.append(strs[i]);
+        }
+        return str.toString();
+    }
+
+    private static String[] copyString(String[] str1) {
+        String[] ans = new String[str1.length];
+        for (int i = 0; i < str1.length; i++) {
+            ans[i] = str1[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int arrLen = 3;
+        int strLen = 4;
+        int times = 10000;
+        for (int i = 0; i < times; i++) {
+            String[] str1 = generateRandomStringArray(arrLen, strLen);
+            String[] str2 = copyString(str1);
+            if (!lowestString1(str1).equals(lowestString2(str2))) {
+                System.out.println("str1" + Arrays.toString(str1));
+                System.out.println("str2" + Arrays.toString(str2));
+                System.out.println("Oop!");
+            }
+        }
+        System.out.println("finish!!");
+    }
+
 
 
 }
