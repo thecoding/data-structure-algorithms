c934c5ebb73364078b63d88716009abb5c0f1407
Mirko <515456737@qq.com>
2022-01-05 22:57:17 +0800

    26

---
+++ src/leetcode/arr/Q28.java
@@ -0,0 +1,34 @@
+package leetcode.arr;
+
+
+/**
+ * 实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列（即，组合出下一个更大的整数）。
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ * 必须 原地 修改，只允许使用额外常数空间。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/next-permutation
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：[1,3,2]
+ *
+ * 输入：nums = [3,2,1]
+ * 输出：[1,2,3]
+ *
+ * 输入：nums = [1,1,5]
+ * 输出：[1,5,1]
+ */
+public class Q28 {
+
+    public static void main(String[] args) {
+        int[] n = {1};
+//        int[] n = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println();
+
+    }
+
+
+    public static void nextPermutation(int[] nums) {
+
+    }
+}
