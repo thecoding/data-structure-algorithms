f633c31e5d9ef78cf2539ebdb9c9cf920aede8e1
zhouchao53381 <515456737@qq.com>
2022-10-11 15:48:16 +0800

    CardsInLine

--- src/com/dp/CardsInLine.java
+++ src/com/dp/CardsInLine.java
@@ -11,6 +11,7 @@ public class CardsInLine {
         System.out.println(win3(arr));
     }
 
+    // 暴力递归
     private static int win1(int[] arr) {
         if (null == arr || arr.length == 0) {
             return 0;
@@ -40,6 +41,7 @@ public class CardsInLine {
         return Math.min(p1, p2);
     }
 
+    // 增加计算缓存
     private static int win2(int[] arr) {
         if (null == arr || arr.length == 0) {
             return 0;
@@ -82,8 +84,31 @@ public class CardsInLine {
         return gArr[L][R];
     }
 
+    // 难点：从 win2 过渡到 win3 需要从win2中抽离，而且是通过二维数组构建，需要有一定的思维能力
+    // 该方法在原方法中抽离 win2 中 f2 g2中看到具体的值是从怎么来的
+    // g -> f2(arr, L + 1, R, fArr, gArr); ->  fArr[L + 1][R]
+    // g -> f2f2(arr, L, R - 1, fArr, gArr); ->  fArr[L][R - 1]
     private static int win3(int[] arr) {
-        return 0;
+        if (null == arr || arr.length == 0) {
+            return 0;
+        }
+        int N = arr.length;
+        int[][] fArr = new int[N][N];
+        int[][] gArr = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            fArr[i][i] = arr[i];
+        }
+        for (int start = 1; start < N; start++) {
+            int L = 0;
+            int R = start;
+            while (R < N) {
+                fArr[L][R] = Math.max(arr[L] + gArr[L + 1][R], arr[R] + gArr[L][R - 1]);
+                gArr[L][R] = Math.min(fArr[L + 1][R], fArr[L][R - 1]);
+                L++;
+                R++;
+            }
+        }
+        return Math.max(fArr[0][N - 1], gArr[0][N - 1]);
     }
 
 }
