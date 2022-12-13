6ecebf0acf66d64225feb6cf741a64bd5ab7927e
zhouchao53381 <515456737@qq.com>
2022-12-13 15:30:56 +0800

    dp2

--- src/com/dp/C5_StickersToSpellWord.java
+++ src/com/dp/C5_StickersToSpellWord.java
@@ -1,4 +1,157 @@
 package com.dp;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/stickers-to-spell-word
+ */
 public class C5_StickersToSpellWord {
+
+    public static void main(String[] args) {
+        String[] strArr = {"niaad","aabbc"};
+        String target = "bbccc";
+        System.out.println("minSticker1 -> "  + minStickers1(strArr, target));
+        System.out.println("minSticker2 -> " + minStickers2(strArr, target));
+        System.out.println("minSticker2 -> " + minStickers3(strArr, target));
+    }
+
+
+    public static int minStickers1(String[] stickers, String target) {
+        int ans = process1(stickers, target);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    // 一次用第一张去减，看看目标有没有减少
+    private static int process1(String[] stickers, String target) {
+        if (target.length() == 0) {
+            return 0;
+        }
+        int min = Integer.MAX_VALUE;
+        for (String sticker : stickers) {
+            String rest = reduce(target, sticker);
+            if (rest.length() != target.length()) {
+                min = Math.min(min, process1(stickers, rest));
+            }
+        }
+        return min + (min == Integer.MAX_VALUE ? 0 : 1);
+    }
+
+    private static String reduce(String target, String sticker) {
+        char[] targetChar = target.toCharArray();
+        char[] stickerChar = sticker.toCharArray();
+        int[] count = new int[26];
+        for (char c : targetChar) {
+            count[c - 'a']++;
+        }
+        for (char c : stickerChar) {
+            count[c - 'a']--;
+        }
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > 0) {
+                for (int j = 0; j < count[i]; j++) {
+                    builder.append((char) (i + 'a'));
+                }
+            }
+        }
+        return builder.toString();
+    }
+
+
+    private static int minStickers2(String[] stickers, String target) {
+        int[][] stickerArr = new int[stickers.length][26];
+        for (int i = 0; i < stickerArr.length; i++) {
+            for (char stickerChar : stickers[i].toCharArray()) {
+                stickerArr[i][stickerChar - 'a']++;
+            }
+        }
+        int ans = process2(stickerArr, target);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+
+    // 目的：将target逐次消除？
+    // target每次都是减少的，
+    public static int process2(int[][] stickers, String target) {
+        // base case
+        if (target.length() == 0) {
+            return 0;
+        }
+        char[] targetArr = target.toCharArray();
+        int[] targetNums = new int[26]; // 序号对应的字母个数
+        for (char c : targetArr) {
+            targetNums[c - 'a']++;
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < stickers.length; i++) {
+            // 取出第i个sticker，一次去扣减target
+            int[] sticker = stickers[i];
+            // targetArr[0] - 'a' 表示目标target第一个字母
+            // sticker[targetArr[0] - 'a'] > 0 表示当前字母可以去消除target中的第一个
+            if (sticker[targetArr[0] - 'a'] > 0) {
+                StringBuilder builder = new StringBuilder();
+                for (int j = 0; j < 26; j++) {
+                    if (targetNums[j] > 0) {
+                        int nums = targetNums[j] - sticker[j];
+                        for (int k = 0; k < nums; k++) {
+                            builder.append((char) (j + 'a'));
+                        }
+                    }
+                }
+                String rest = builder.toString();
+                min = Math.min(min, process2(stickers, rest));
+            }
+        }
+        return min + (min == Integer.MAX_VALUE ? 0 : 1);
+    }
+
+
+    // 根据minStickers2改成dp
+    private static int minStickers3(String[] stickers, String target) {
+        int[][] stickerArr = new int[stickers.length][26];
+        for (int i = 0; i < stickerArr.length; i++) {
+            for (char stickerChar : stickers[i].toCharArray()) {
+                stickerArr[i][stickerChar - 'a']++;
+            }
+        }
+        Map<String, Integer> dp = new HashMap<>();
+        dp.put("", 0);
+        int ans = process3(stickerArr, target, dp);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    private static int process3(int[][] stickers, String target, Map<String,Integer> dp) {
+        if(dp.containsKey(target)){
+            return dp.get(target);
+        }
+        char[] targetArr = target.toCharArray();
+        int[] targetNums = new int[26]; // 序号对应的字母个数
+        for (char c : targetArr) {
+            targetNums[c - 'a']++;
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < stickers.length; i++) {
+            int[] sticker = stickers[i];
+            if (sticker[targetArr[0] - 'a'] > 0) {
+                StringBuilder builder = new StringBuilder();
+                for (int j = 0; j < 26; j++) {
+                    if (targetNums[j] > 0) {
+                        int nums = targetNums[j] - sticker[j];
+                        for (int k = 0; k < nums; k++) {
+                            builder.append((char) (j + 'a'));
+                        }
+                    }
+                }
+                String rest = builder.toString();
+                min = Math.min(min, process2(stickers, rest));
+            }
+        }
+        int ans = min + (min == Integer.MAX_VALUE ? 0 : 1);
+        dp.put(target, ans);
+        return ans;
+    }
+
+
+
 }
--- src/com/dp/C6_LongestCommonSubsequence.java
+++ src/com/dp/C6_LongestCommonSubsequence.java
@@ -6,17 +6,79 @@ package com.dp;
  * 样本尝试：特别需要讨论结尾如何
  * 范围模型：需要讨论开头如何、结尾如何
  * 从左往右：
- * 业务显示模型
+ * 业务显示模型：
+ * 空间压缩  --> 根据依赖关系、模型观察
+ *
+ * 链接：https://leetcode.com/problems/longest-common-subsequence/
  */
 public class C6_LongestCommonSubsequence {
 
 
-    public static int longestCommonSubsequence(String str1, String str2) {
-        return -1;
-    }
-
-
     public static void main(String[] args) {
-
+        String str1 = "abcccassdc";
+        String str2 = "jjlcdaasc";
+        System.out.println(longestCommonSubsequence(str1, str2));
+        System.out.println(longestCommonSubsequence2(str1, str2));
     }
+
+    public static int longestCommonSubsequence(String str1, String str2) {
+        if (str1 == null || str1.length() == 0 || str2 == null || str2.length() == 0) {
+            return 0;
+        }
+        char[] str1Arr = str1.toCharArray();
+        char[] str2Arr = str2.toCharArray();
+        return process1(str1Arr, str2Arr, str1Arr.length - 1, str2Arr.length - 1);
+    }
+
+    private static int process1(char[] str1Arr, char[] str2Arr, int i, int j) {
+        if (i == 0 && j == 0) {
+            return str1Arr[i] == str2Arr[j] ? 1 : 0;
+        } else if (i == 0) {
+            if (str1Arr[i] == str2Arr[j]) {
+                return 1;
+            }else{
+                return process1(str1Arr, str2Arr, i, j - 1);
+            }
+        } else if (j == 0) {
+            if (str1Arr[i] == str2Arr[j]) {
+                return 1;
+            }else{
+                return process1(str1Arr, str2Arr, i - 1, j);
+            }
+        }else{
+            int p1 = process1(str1Arr, str2Arr, i - 1, j);
+            int p2 = process1(str1Arr, str2Arr, i, j - 1);
+            int p3 = str1Arr[i] == str2Arr[j] ? (1 + process1(str1Arr, str2Arr, i - 1, j - 1)) : 0;
+            return Math.max(p1, Math.max(p2, p3));
+        }
+    }
+
+
+    public static int longestCommonSubsequence2(String str1, String str2) {
+        if (str1 == null || str1.length() == 0 || str2 == null || str2.length() == 0) {
+            return 0;
+        }
+        char[] str1Arr = str1.toCharArray();
+        char[] str2Arr = str2.toCharArray();
+        int[][] dp = new int[str1Arr.length][str2Arr.length];
+
+        for (int i = 0; i < str1Arr.length; i++) {
+            dp[i][0] = str1Arr[i] == str2Arr[0] ? 1 : 0;
+        }
+        for (int i = 0; i < str2Arr.length; i++) {
+            dp[0][i] = str1Arr[0] == str2Arr[i] ? 1 : 0;
+        }
+
+        for (int i = 1; i < str1Arr.length; i++) {
+            for (int j = 1; j < str2Arr.length; j++) {
+                int p1 = dp[i-1][j];
+                int p2 = dp[i][j - 1];
+                int p3 = str1Arr[i] == str2Arr[j] ? 1 + dp[i - 1][j - 1] : 0;
+                dp[i][j] = Math.max(p1, Math.max(p2, p3));
+            }
+        }
+        return dp[str1Arr.length - 1][str2Arr.length - 1];
+    }
+
+
 }
